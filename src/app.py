4e67b2325c853725f1d36c9ab529564752fe287e
Copter <motto5465@gmail.com>
2024-12-10 07:06:44 +0700

    fix - CORS, enviroment variable

--- src/app.py
+++ src/app.py
@@ -27,14 +27,9 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = [
-    "http://localhost:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
